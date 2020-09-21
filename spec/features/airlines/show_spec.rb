c7c509bb6266e674fcf0dda99d40f6cbb7e4ed4b
Chris Allbritton <cla.inbox@gmail.com>
2020-09-21 13:48:54 -0400

    Minor refactoring

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -14,22 +14,15 @@ RSpec.describe "When I visit an airline's show page," do
     @passenger_3 = Passenger.create(name: "Charles Bob", age: 33)
 
     FlightPassenger.create(flight_id: @flight1.id, passenger_id: @passenger_1.id)
+    FlightPassenger.create(flight_id: @flight3.id, passenger_id: @passenger_1.id)
     FlightPassenger.create(flight_id: @flight1.id, passenger_id: @passenger_2.id)
     FlightPassenger.create(flight_id: @flight2.id, passenger_id: @passenger_3.id)
   end
 
   it "I see a unique list of passengers that have flights from the airline" do
     visit "/airlines/#{@airline1.id}"
-
     expect(page).to have_content(@passenger_1.name)
     expect(page).to have_content(@passenger_2.name)
     expect(page).to have_content(@passenger_3.name)
   end
 end
-
-
-
-# User Story 2, Airlines Show Page
-# As a visitor
-# When I visit an airlines show page ('/airlines/:id')
-# I see a unique list of passengers that have flights from that airline
